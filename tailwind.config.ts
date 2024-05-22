8b62a3703e119b1f39cc213dec1ac34e7a472888
Josh Corbett <joshwcorbett@icloud.com>
2024-05-22 15:00:54 -0600

    feat: :sparkles: use brand colors in tailwind classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,14 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./*/*.{liquid,json,js}'],
-  theme: {},
+  theme: {
+    colors: {
+      'primary-bg': 'hsl(var(--color-primary-bg) / <alpha-value>)',
+      'primary-fg': 'hsl(var(--color-primary-fg) / <alpha-value>)',
+      'secondary-bg': 'hsl(var(--color-secondary-bg) / <alpha-value>)',
+      'secondary-fg': 'hsl(var(--color-secondary-fg) / <alpha-value>)',
+      'accent-bg': 'hsl(var(--color-accent-bg) / <alpha-value>)',
+    }
+  },
   plugins: [],
 } satisfies Config;

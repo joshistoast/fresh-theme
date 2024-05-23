d76306e29867dc25497877ef253f9a2ce35bcc3b
Josh Corbett <joshwcorbett@icloud.com>
2024-05-23 13:16:13 -0600

    feat: :sparkles: make `base-*` classes react to light/dark backgrounds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,21 +4,21 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./*/*.{liquid,json,js}'],
   theme: {
+    colors: {
+      'primary': 'hsl(var(--color-primary) / <alpha-value>)',
+      'primary-content': 'hsl(var(--color-primary-content) / <alpha-value>)',
+      'secondary': 'hsl(var(--color-secondary) / <alpha-value>)',
+      'secondary-content': 'hsl(var(--color-secondary-content) / <alpha-value>)',
+      'accent': 'hsl(var(--color-accent) / <alpha-value>)',
+      'accent-content': 'hsl(var(--color-accent-content) / <alpha-value>)',
+      'neutral': 'hsl(var(--color-neutral) / <alpha-value>)',
+      'neutral-content': 'hsl(var(--color-neutral-content) / <alpha-value>)',
+      'base-100': 'hsl(var(--color-base-100) / <alpha-value>)',
+      'base-200': 'hsl(var(--color-base-200) / <alpha-value>)',
+      'base-300': 'hsl(var(--color-base-300) / <alpha-value>)',
+      'base-content': 'hsl(var(--color-base-content) / <alpha-value>)',
+    },
     extend: {
-      colors: {
-        'primary': 'hsl(var(--color-primary) / <alpha-value>)',
-        'primary-content': 'hsl(var(--color-primary-content) / <alpha-value>)',
-        'secondary': 'hsl(var(--color-secondary) / <alpha-value>)',
-        'secondary-content': 'hsl(var(--color-secondary-content) / <alpha-value>)',
-        'accent': 'hsl(var(--color-accent) / <alpha-value>)',
-        'accent-content': 'hsl(var(--color-accent-content) / <alpha-value>)',
-        'neutral': 'hsl(var(--color-neutral) / <alpha-value>)',
-        'neutral-content': 'hsl(var(--color-neutral-content) / <alpha-value>)',
-        'base-100': 'hsl(var(--color-base-100) / <alpha-value>)',
-        'base-200': 'hsl(var(--color-base-200) / <alpha-value>)',
-        'base-300': 'hsl(var(--color-base-300) / <alpha-value>)',
-        'base-content': 'hsl(var(--color-base-content) / <alpha-value>)',
-      },
       typography: ({ theme }) => ({
         DEFAULT: {
           css: {

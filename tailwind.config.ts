06e4e82e75645db5f141b5a718d124becb780a66
Josh Corbett <joshwcorbett@icloud.com>
2024-05-23 12:53:26 -0600

    feat: :sparkles: add color schemes w/relative tailwind classes
    Each color scheme gets tailwind color classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,47 @@ import typography from '@tailwindcss/typography';
 export default {
   content: ['./*/*.{liquid,json,js}'],
   theme: {
-    colors: {
-      'primary-bg': 'hsl(var(--color-primary-bg) / <alpha-value>)',
-      'primary-fg': 'hsl(var(--color-primary-fg) / <alpha-value>)',
-      'secondary-bg': 'hsl(var(--color-secondary-bg) / <alpha-value>)',
-      'secondary-fg': 'hsl(var(--color-secondary-fg) / <alpha-value>)',
-      'accent-bg': 'hsl(var(--color-accent-bg) / <alpha-value>)',
+    extend: {
+      colors: {
+        'primary': 'hsl(var(--color-primary) / <alpha-value>)',
+        'primary-content': 'hsl(var(--color-primary-content) / <alpha-value>)',
+        'secondary': 'hsl(var(--color-secondary) / <alpha-value>)',
+        'secondary-content': 'hsl(var(--color-secondary-content) / <alpha-value>)',
+        'accent': 'hsl(var(--color-accent) / <alpha-value>)',
+        'accent-content': 'hsl(var(--color-accent-content) / <alpha-value>)',
+        'neutral': 'hsl(var(--color-neutral) / <alpha-value>)',
+        'neutral-content': 'hsl(var(--color-neutral-content) / <alpha-value>)',
+        'base-100': 'hsl(var(--color-base-100) / <alpha-value>)',
+        'base-200': 'hsl(var(--color-base-200) / <alpha-value>)',
+        'base-300': 'hsl(var(--color-base-300) / <alpha-value>)',
+        'base-content': 'hsl(var(--color-base-content) / <alpha-value>)',
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.base-content/80'),
+            a: {
+              color: theme('colors.primary/100'),
+              textDecoration: 'underline',
+              '&:hover': {
+                textDecoration: 'none',
+              },
+            },
+            'h1,h2,h3,h4': {
+              color: theme('colors.base-content/100'),
+            },
+            code: {
+              color: theme('colors.base-content/100'),
+            },
+            'blockquote p:first-of-type::before': {
+              content: 'none',
+            },
+            'blockquote p:last-of-type::after': {
+              content: 'none',
+            },
+          },
+        },
+      }),
     }
   },
   plugins: [
